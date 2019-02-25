1f4052e9ca60eef3fa2ef59a95293c14cafa324a
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2019-02-25 22:59:28 +0100

    use format on file2echo.

--- file2echo.py
+++ file2echo.py
@@ -3,13 +3,27 @@
 import os
 import sys
 
+if len(sys.argv) < 2:
+    print('Convert a binary file to printable hex chars (like \\x41\\x42)')
+    print()
+    print('Usage : file2echo <file> [<line_size>]'.format(sys.argv[0]))
+    print('\tfile : file 2 convert')
+    print('\tnumber of bytes by line (64 by default)')
+    quit()
+
 file = sys.argv[1]
-blocksize = sys.argv[2]
+if len(sys.argv) > 2:
+    blocksize = int(sys.argv[2])
+else:
+    blocksize = 64
 
 with open(file, 'rb') as f:
-    data = f.read(64)
-    for b in data:
-        sys.stdout.write(hex(b).replace('0x', "\\x"))
-    sys.stdout.write('\n')
-    sys.stdout.flush()
+    while True:
+        data = f.read(blocksize)
+        if len(data) == 0:
+            break
+        for b in data:
+            sys.stdout.write("\\x{:0>2x}".format(b))
+        sys.stdout.write('\n')
+        sys.stdout.flush()
 
