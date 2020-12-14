d537c801bd2e4395b87c60dcde27cf5de9db1cd8
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2020-12-14 23:25:49 +0100

    webshell : add html2text option

--- webshell.py
+++ webshell.py
@@ -2,6 +2,7 @@
 
 import requests
 import argparse
+import html2text
 import sys
 
 
@@ -9,6 +10,7 @@ parser = argparse.ArgumentParser('get a webshell with url, and use <param> as co
 parser.add_argument('--header', action='append', help='add header')
 parser.add_argument('--param', default='cmd', help='param with command to use')
 parser.add_argument('--cmd', help='command to send')
+parser.add_argument('--html2text', action='store_true', help='print html on text mode')
 parser.add_argument('url')
 
 args = parser.parse_args()
@@ -21,13 +23,15 @@ cmd = args.cmd
 
 
 
-def execute(cmd, headers):
+def execute(cmd, headers, h2t):
 	
     r = requests.post(url, data={param: cmd}, headers=headers)
  
     print(r.request.url)
-    t = r.text
 
+    t = r.text
+    if h2t:
+        t = html2text.html2text(t)
     print(t)
 
 
@@ -39,11 +43,11 @@ if args.header:
         headers[key] = value
 
 if cmd is not None:
-    execute(cmd, headers)
+    execute(cmd, headers, args.html2text)
 else:	
     while True:
         c = input('--> ')
-        execute(c, headers)
+        execute(c, headers, args.html2text)
     
 
 
